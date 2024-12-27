24441792ff643827122811c07e0eb67a14fefa4a
chaeelin <chaeee0123@gmail.com>
2024-12-28 00:42:47 +0900

    A.java C.java 수정

--- src/homework/A.java
+++ src/homework/A.java
@@ -2,6 +2,6 @@ package homework;
 
 public class A {
 	public static void main (String[] args) {
-        System.out.println("This is the modified version of A.java in devB branch.");
+        System.out.println("This is the modified version of A.java in devC branch.");
 	}
 }
