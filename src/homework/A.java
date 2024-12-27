182809f9f2f3a691fefd64e4c34da17410e5737c
chaeelin <chaeee0123@gmail.com>
2024-12-28 00:43:49 +0900

    A.java 수정

--- src/homework/A.java
+++ src/homework/A.java
@@ -2,6 +2,6 @@ package homework;
 
 public class A {
 	public static void main (String[] args) {
-        System.out.println("This is the initial version of A.java.");
+        System.out.println("This is the modified version of A.java in master branch.");
 	}
 }
